37e3f69ddc8750a555d077e46c300db6fb73db0b
Isabella Naranjo Vargas <cnscisabellanaranjo@gmail.com>
2022-03-01 12:18:15 -0500

    control de ui

---
+++ src/control/Registro.java
@@ -0,0 +1,24 @@
+package control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Registro {
+
+    @FXML
+    private TextField idStudentTF;
+
+    @FXML
+    private TextField nameStudentTF;
+
+    @FXML
+    private Button registrarBT;
+
+    @FXML
+    void registrar(ActionEvent event) {
+
+    }
+
+}
